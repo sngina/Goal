c4a6bd38ef521a397a2461eb5eef9364d7c143ea
sngina <sngina707@gmail.com>
2021-06-12 16:47:14 +0300

    installed flask login

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import config_options
+from flask_login import LoginManager
+
 
 db = SQLAlchemy()
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
 
 # function of creating app.
 def create_app(config_name):
@@ -11,9 +18,15 @@ def create_app(config_name):
     #creating the app configurations.
     app.config.from_object(config_options[config_name])
 
-    
+
     #Initializing flask extensions
     db.init_app(app)
+    login_manager.init_app(app)
+
+    
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
 
     
     return app
