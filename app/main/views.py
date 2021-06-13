8cfb727e3e81d60452b8d6a50a0b250ad2d65fb0
sngina <sngina707@gmail.com>
2021-06-13 09:56:33 +0300

    add an html file

---
+++ app/main/views.py
@@ -0,0 +1,12 @@
+from flask import render_template,request,redirect,url_for,abort
+from ..models import User
+
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
