b59350c29369b544e55977e8109bf8ba81ea8eb4
sngina <sngina707@gmail.com>
2021-06-15 10:51:36 +0300

    change production

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_USERNAME = os.environ.get("stella.kimaru@student.moringaschool.com")
+    MAIL_PASSWORD = os.environ.get("kimaru@2015!")
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
 
