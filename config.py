8d5a6b74d435804636bd5eb3797d52893132bd2b
sngina <sngina707@gmail.com>
2021-06-15 11:35:48 +0300

    change manage file into production

--- config.py
+++ config.py
@@ -3,18 +3,21 @@ import os
 
 class Config:
     SECRET_KEY = 'YOUcantHackThis'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:malika@localhost/one_minute_pitch'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("stella.kimaru@student.moringaschool.com")
-    MAIL_PASSWORD = os.environ.get("kimaru@2015!")
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    # simple mde  configurations
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
 
 class ProdConfig(Config):
       SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+      if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+            SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
 
+      
 class TestConfig(Config):
       SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:malika@localhost/pitch'
 class DevConfig(Config):
