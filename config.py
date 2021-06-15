990caa5d6e2fe1b99c0612b2f89e226dade704b7
sngina <sngina707@gmail.com>
2021-06-15 12:37:38 +0300

    change to development

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config:
     SIMPLEMDE_USE_CDN = True
 
 class ProdConfig(Config):
-      SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+      SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL","")
       if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
             SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
 
